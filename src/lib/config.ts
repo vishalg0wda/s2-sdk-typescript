0f2fb5ab0566a38c6000b96d09ac658f0ca739e8
Mehul Arora <mehul_arora@live.com>
2025-01-16 13:49:17 -0500

    chore: update

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -54,7 +54,7 @@ export function serverURLFromOptions(options: SDKOptions): URL | null {
 export const SDK_METADATA = {
   language: "typescript",
   openapiDocVersion: "1.0.0",
-  sdkVersion: "0.1.0",
+  sdkVersion: "0.1.2",
   genVersion: "2.493.21",
-  userAgent: "speakeasy-sdk/typescript 0.1.0 2.493.21 1.0.0 streamstore",
+  userAgent: "speakeasy-sdk/typescript 0.1.2 2.493.21 1.0.0 streamstore",
 } as const;
